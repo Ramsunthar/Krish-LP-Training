43950fec847d5ca48196c8ddf0c41db7da6fcde7
ramsunthar <ramsunthar.s@gmail.com>
2021-07-01 22:53:04 +0530

    validation rework

--- NestJS Project/pet-clinic-service/src/owners/owners.controller.ts
+++ NestJS Project/pet-clinic-service/src/owners/owners.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Owner } from 'src/owners/schema/Owner.schema';
 import { OwnerCreateDto } from './dto/OwnerCreate.dto';
 import { OwnerUpdateDto } from './dto/OwnerUpdate.dto';
@@ -16,7 +16,7 @@ export class OwnersController {
 
     @Get('/:id')
     async findOne(@Param('id') id: string) : Promise<Owner> {
-        return await this.ownersService.findById(id);
+        return await this.ownersService.findById(id).catch(err => {throw new NotFoundException(`${id} is not found`)})
     }
 
     @Post()
@@ -28,11 +28,11 @@ export class OwnersController {
 
     @Put('/:id')
     async updateOwner(@Param('id') id: string,@Body() ownerUpdateDto: OwnerUpdateDto) : Promise<Owner> {
-        return await this.ownersService.updateOwner(id,ownerUpdateDto);
+        return await this.ownersService.updateOwner(id,ownerUpdateDto).catch(err => {throw new NotFoundException(`${id} is not found`)})
     }
 
     @Delete('/:id')
-    async deleteOwner(@Param('id') id: string): Promise<Owner> {
-        return await this.ownersService.deleteOwner(id);
+    async deleteOwner(@Param('id') id: string) {
+        return await this.ownersService.deleteOwner(id).catch(err => {throw new NotFoundException(`${id} is not found`)})
     }
 }
